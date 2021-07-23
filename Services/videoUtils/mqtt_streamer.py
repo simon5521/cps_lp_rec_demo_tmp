cd6f9f2d670a0c6e497413934c3d5c58694858a9
nukleari <nukleari591@gmail.com>
2021-07-23 20:15:17 +0200

    removed global variables

--- Services/videoUtils/mqtt_streamer.py
+++ Services/videoUtils/mqtt_streamer.py
@@ -1,107 +1,107 @@
 #!/usr/bin/python3
-import time
 import json
-import paho.mqtt.client as paho
+import time
 from multiprocessing import Process, Queue
 
-mqtt_input_buffer = Queue()
-mqtt_output_buffer = Queue()
-mqtt_telemetry_buffer = Queue(1)
-mqtt_topic_sub = None
-mqtt_topic_pub = None
-host_id = ''
-mqtt_logging_buffer = None
+import paho.mqtt.client as paho
 
-#define callback
-def on_message(client, userdata, message):
-    global mqtt_input_buffer
+
+# define callback
+def on_message(client, userdata, message, host_id, input_buffer, logging_buffer):
     top = message.topic
     msg = str(message.payload.decode("utf-8"))
     data = json.loads(msg)
     if((data["source"] != host_id) and ((data["destination"] == "") or (data["destination"] == host_id))):
         try:
-            mqtt_input_buffer.put_nowait(data)
+            input_buffer.put_nowait(data)
         except:
-            mqtt_input_buffer.get()
-            mqtt_input_buffer.put(data)
-            if mqtt_logging_buffer != None:
-                mqtt_logging_buffer.put({'measurement': '', 'component': 'mqtt_streamer', 'data': 'mqtt_input_buffer'})
+            input_buffer.get()
+            input_buffer.put(data)
+            if logging_buffer != None:
+                logging_buffer.put(
+                    {'measurement': '', 'component': 'mqtt_streamer', 'data': 'mqtt_input_buffer'})
 
-def on_connect(client, userdata, flags, rc):
-    global mqtt_topic_sub
-    if rc==0:
-        client.subscribe(mqtt_topic_sub)
-        client.subscribe(f'{mqtt_topic_sub}-{host_id}')
+
+def on_connect(client, userdata, flags, rc, host_id, topic_sub):
+    if rc == 0:
+        client.subscribe(topic_sub)
+        client.subscribe(f'{topic_sub}-{host_id}')
     else:
-        print("Bad connection Returned code=",rc)
+        print("Bad connection Returned code=", rc)
 
-def start_subscriber(broker, port, username, password):
-    client = paho.Client() 
-    client.on_connect=on_connect
-    client.on_message=on_message
+
+def start_subscriber(host_id, broker, port, topic_sub, username, password, input_buffer, logging_buffer):
+    client = paho.Client()
+    client.on_connect = lambda client, userdata, flags, rc: on_connect(
+        client, userdata, flags, rc, host_id, topic_sub)
+    client.on_message = lambda client, userdata, message: on_message(
+        client, userdata, message, host_id, input_buffer, logging_buffer)
     if(username != None):
         client.username_pw_set(username, password)
     client.connect(broker, port)
     client.loop_forever()
 
-def start_telemetry_publisher(broker, port, username, password):
-    global mqtt_input_buffer
-    client = paho.Client() 
+
+def start_telemetry_publisher(host_id, broker, port, topic_sub, username, password, input_buffer):
+    client = paho.Client()
     if(username != None):
         client.username_pw_set(username, password)
     client.connect(broker, port)
     while True:
-        data = {'qsize' : mqtt_input_buffer.qsize(),
-                'topic' : f'{mqtt_topic_sub}-{host_id}'}
+        data = {'qsize': input_buffer.qsize(),
+                'topic': f'{topic_sub}-{host_id}'}
         message = json.dumps(data)
-        client.publish(f'{mqtt_topic_sub}-telemetry', message)
+        client.publish(f'{topic_sub}-telemetry', message)
         time.sleep(.5)
 
 
-telemetry_table = {}
-def on_message_telemetry(client, userdata, message):
-    global mqtt_telemetry_buffer, telemetry_table
+def on_message_telemetry(client, userdata, message, host_id, telemetry_buffer, telemetry_table):
     top = message.topic
     msg = str(message.payload.decode("utf-8"))
     data = json.loads(msg)
     telemetry_table[data['topic']] = data
     telemetry_table[data['topic']]['last_update_time'] = time.time()
     while True:
-        min_key = min(telemetry_table, key=lambda k: telemetry_table[k]['qsize'])
+        min_key = min(telemetry_table,
+                      key=lambda k: telemetry_table[k]['qsize'])
         if(time.time() - telemetry_table[min_key]['last_update_time'] > 10):
             telemetry_table.pop(min_key)
         else:
             try:
-                mqtt_telemetry_buffer.put_nowait(telemetry_table[min_key])
+                telemetry_buffer.put_nowait(telemetry_table[min_key])
             except:
                 try:
-                    mqtt_telemetry_buffer.get_nowait()
+                    telemetry_buffer.get_nowait()
                 except:
                     pass
-                mqtt_telemetry_buffer.put(telemetry_table[min_key])
+                telemetry_buffer.put(telemetry_table[min_key])
             break
         if(len(telemetry_table.keys()) == 0):
             break
 
-def on_connect_telemetry(client, userdata, flags, rc):
-    global mqtt_topic_pub
-    if rc==0:
-        client.subscribe(f'{mqtt_topic_pub}-telemetry')
-    else:
-        print("Bad connection Returned code=",rc)
 
-def start_telemetry_subscriber(broker, port, username, password):
-    client = paho.Client() 
-    client.on_connect=on_connect_telemetry
-    client.on_message=on_message_telemetry
+def on_connect_telemetry(client, userdata, flags, rc, host_id, topic_pub):
+    if rc == 0:
+        client.subscribe(f'{topic_pub}-telemetry')
+    else:
+        print("Bad connection Returned code=", rc)
+
+
+def start_telemetry_subscriber(host_id, broker, port, topic_pub, username, password, output_buffer, telemetry_buffer):
+    client = paho.Client()
+    telemetry_table = {}
+    client.on_connect = lambda client, userdata, flags, rc: on_connect_telemetry(
+        client, userdata, flags, rc, host_id, topic_pub)
+    client.on_message = lambda client, userdata, message: on_message_telemetry(
+        client, userdata, message, host_id, telemetry_buffer, telemetry_table)
     if(username != None):
         client.username_pw_set(username, password)
     client.connect(broker, port)
     client.loop_forever()
 
-def start_publisher(broker, port, topic_pub, username, password):
-    global mqtt_output_buffer, mqtt_telemetry_buffer
-    client = paho.Client() 
+
+def start_publisher(host_id, broker, port, topic_pub, username, password, output_buffer, telemetry_buffer):
+    client = paho.Client()
     if(username != None):
         client.username_pw_set(username, password)
     client.connect(broker, port)
@@ -109,42 +109,45 @@ def start_publisher(broker, port, topic_pub, username, password):
     last_update_time = 0
     while True:
         try:
-            telemetry = mqtt_telemetry_buffer.get_nowait()
+            telemetry = telemetry_buffer.get_nowait()
             topic = telemetry['topic']
             last_update_time = telemetry['last_update_time']
         except:
             pass
 
         if(time.time() - last_update_time < 10):
-            data = mqtt_output_buffer.get()
+            data = output_buffer.get()
             data["source"] = str(host_id)
             if 'destination' not in data:
                 data['destination'] = ''
             message = json.dumps(data)
-            client.publish(topic,message)
+            client.publish(topic, message)
 
         time.sleep(0.01)
 
-def  start_mqtt_streamer(nodeid, broker = "127.0.0.1", port = 1883, topic_pub = None, topic_sub = None, username = None, password= None, input_buffer_size = 10, output_buffer_size = 10, logging_buffer=None):
-    global mqtt_input_buffer, mqtt_output_buffer, mqtt_topic_sub, mqtt_topic_pub, host_id, mqtt_logging_buffer
+
+def start_mqtt_streamer(nodeid, broker="127.0.0.1", port=1883, topic_pub=None, topic_sub=None, username=None, password=None, input_buffer_size=10, output_buffer_size=10, logging_buffer=None):
+    global mqtt_logging_buffer
     host_id = nodeid
-    mqtt_topic_sub = topic_sub
-    mqtt_topic_pub = topic_pub
-    mqtt_input_buffer = Queue(input_buffer_size)
-    mqtt_output_buffer = Queue(output_buffer_size)
-    mqtt_logging_buffer = logging_buffer
+    input_buffer = Queue(input_buffer_size)
+    output_buffer = Queue(output_buffer_size)
+    telemetry_buffer = Queue(1)
     if topic_sub != None:
-        mqtt_sub_client_process = Process(name='mqtt client sub' + str(broker), target=start_subscriber, args=(broker, port, username, password))
+        mqtt_sub_client_process = Process(name='mqtt client sub' + str(broker), target=start_subscriber, args=(
+            host_id, broker, port, topic_sub, username, password, input_buffer, logging_buffer))
         mqtt_sub_client_process.daemon = True
         mqtt_sub_client_process.start()
-        mqtt_telemetry_publisher_process = Process(name='mqtt start telemetry publisher' + str(broker), target=start_telemetry_publisher, args=(broker, port, username, password))
+        mqtt_telemetry_publisher_process = Process(name='mqtt start telemetry publisher' + str(
+            broker), target=start_telemetry_publisher, args=(host_id, broker, port, topic_sub, username, password, input_buffer))
         mqtt_telemetry_publisher_process.daemon = True
         mqtt_telemetry_publisher_process.start()
     if topic_pub != None:
-        mqtt_pub_client_process = Process(name='mqtt client pub' + str(broker), target=start_publisher, args=(broker, port, topic_pub, username, password))
+        mqtt_pub_client_process = Process(name='mqtt client pub' + str(broker), target=start_publisher, args=(
+            host_id, broker, port, topic_pub, username, password, output_buffer, telemetry_buffer))
         mqtt_pub_client_process.daemon = True
         mqtt_pub_client_process.start()
-        mqtt_telemetry_subscriberr_process = Process(name='mqtt start telemetry subscriber' + str(broker), target=start_telemetry_subscriber, args=(broker, port, username, password))
+        mqtt_telemetry_subscriberr_process = Process(name='mqtt start telemetry subscriber' + str(
+            broker), target=start_telemetry_subscriber, args=(host_id, broker, port, topic_pub, username, password, output_buffer, telemetry_buffer))
         mqtt_telemetry_subscriberr_process.daemon = True
         mqtt_telemetry_subscriberr_process.start()
-    return mqtt_input_buffer, mqtt_output_buffer
+    return input_buffer, output_buffer
