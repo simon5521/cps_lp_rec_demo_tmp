ea4c543398d5ee4a775cc47a6b11e7835e3bd4f7
simon5521 <simon5521@freemail.hu>
2022-03-29 14:50:56 +0200

    arrowhead service added

--- Services/Arrowhead_Service/test_consumer_start.sh
+++ Services/Arrowhead_Service/test_consumer_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker container stop test-consumer-service
 docker container rm test-consumer-service
-docker run --network="host" --name test-consumer-service test_consumer:initial
+docker run -p 17000:17000 --name test-consumer-service test_consumer:initial
